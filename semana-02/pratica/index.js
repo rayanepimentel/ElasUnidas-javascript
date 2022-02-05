ec3a56f20d885bd4956cf30198f6f0d7c0bc3cda
Rayane <c1325365@interno.bb.com.br>
2022-02-05 18:39:47 -0300

    semana 03

--- semana-02/pratica/index.js
+++ semana-02/pratica/index.js
@@ -1,4 +1,4 @@
-let n = parseInt(prompt("digite um número: "));
+//let n = parseInt(prompt("digite um número: "));
 //prompt("digite um número: ");
 //console.log(typeof(n), n);
 
@@ -26,4 +26,52 @@ function isEven(par) {
     }
 }
 
-console.log(isEven(n));
+//console.log(isEven(n));
+
+//outra maneira simplificada
+
+function isEven(n) {
+    if (n == 0) return true;
+    else if (n == 1) return false;
+    else if (n < 0) return isEven(-n);
+    else return isEven(n - 2)
+}
+
+
+function countBS(value) {
+    for (let i = 0; i < value.length; i++) {
+        //console.log(value)
+        let letra = "B"
+        let cat = value.charAt(letra)
+        console.log(cat)
+        if (cat == letra) {
+            //console.log(cat)
+        }
+    }
+}
+
+//console.log(countBS("BolaDeBmaisb"))
+
+
+function isEvens(n) {
+    if (n == 0) {
+        console.log(1);
+        return true;
+    } else if (n == 1) {
+        console.log(2);
+        return false;
+    } else if (n < 0) {
+        console.log(3);
+        return isEvens(-n);
+    } else {
+        console.log(4);
+        return isEvens(n - 2);
+    }
+}
+//let x = parseInt(prompt("digite um número: "));
+//console.log(isEvens(x));
+// → true
+//console.log(isEvens(75));
+// → false
+//console.log(isEvens(-1));
+// → false
